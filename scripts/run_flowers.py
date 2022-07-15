fb5e8e64aafe4a27c9a4054bdfebc2f67cbff8e8
kinaanaamir <kinaan.aamir96@gmail.com>
2022-07-15 12:18:07 +0200

    added dropout grid search

--- scripts/run_flowers.py
+++ scripts/run_flowers.py
@@ -9,7 +9,8 @@ from itertools import combinations
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dropout = 0.7
+    dropouts = [0, 0.1, 0.3, 0.5, 0.7]
+    n_epochs = 200
     model_dictionary = ModelHelperMethods.make_models_dictionary(device)
     model_to_embedding_dictionary = ModelHelperMethods.get_end_embedding_dictionary(model_dictionary, device)
 
@@ -43,39 +44,41 @@ if __name__ == "__main__":
     for i in list(temp):
         all_models.append([*i])
 
-    acc_and_f1 ={}
-    for model_names in all_models:
+    acc_and_f1 = {}
+    for dropout in dropouts:
+        for model_names in all_models:
 
-        path = "model_weights"
-        for name in model_names:
-            path += "_" + name
-        path = path + "/"
-        if not os.path.exists(weight_path + path):
-            os.mkdir(weight_path + path)
+            path = "model_weights"
+            for name in model_names:
+                path += "_" + name
+            path = path + "_" + str(dropout) + "/"
+            if not os.path.exists(weight_path + path):
+                os.mkdir(weight_path + path)
 
-        model = TinyModel(model_names, total_classes, dropout, model_dictionary, model_to_embedding_dictionary)
-        criterion, optimizer_ft, exp_lr_scheduler = ModelHelperMethods.get_criterion_optimizer_scheduler(model)
-        model = model.to(device)
-        model, train_losses, val_losses = ModelTrainingMethods.train_model(model, criterion, optimizer_ft,
-                                                                           exp_lr_scheduler,
-                                                                           train_dataloader, test_dataloader,
-                                                                           weight_path + path, device, num_epochs=10)
-        print(model_names)
-        final_name = ""
-        for name in model_names:
-            final_name += name + "_"
+            model = TinyModel(model_names, total_classes, dropout, model_dictionary, model_to_embedding_dictionary)
+            criterion, optimizer_ft, exp_lr_scheduler = ModelHelperMethods.get_criterion_optimizer_scheduler(model)
+            model = model.to(device)
+            model, train_losses, val_losses = ModelTrainingMethods.train_model(model, criterion, optimizer_ft,
+                                                                               exp_lr_scheduler,
+                                                                               train_dataloader, test_dataloader,
+                                                                               weight_path + path, device,
+                                                                               num_epochs=n_epochs)
+            print(model_names)
+            final_name = ""
+            for name in model_names:
+                final_name += name + "_"
 
-        final_name = final_name[:-1]
-        acc_and_f1[final_name] = [ModelTrainingMethods.get_acc(model, train_dataloader, device),
-                                  ModelTrainingMethods.get_acc(model, test_dataloader, device),
-                                  ModelTrainingMethods.get_f1_score(model, train_dataloader, device),
-                                  ModelTrainingMethods.get_f1_score(model, test_dataloader, device)]
-        print("Training acc ", acc_and_f1[final_name][0])
-        print("validation acc ", acc_and_f1[final_name][1])
-        print("Training f1 ", acc_and_f1[final_name][2])
-        print("validation f1 ", acc_and_f1[final_name][3])
+            final_name = final_name + str(dropout)
+            acc_and_f1[final_name] = [ModelTrainingMethods.get_acc(model, train_dataloader, device),
+                                      ModelTrainingMethods.get_acc(model, test_dataloader, device),
+                                      ModelTrainingMethods.get_f1_score(model, train_dataloader, device),
+                                      ModelTrainingMethods.get_f1_score(model, test_dataloader, device)]
+            print("Training acc ", acc_and_f1[final_name][0])
+            print("validation acc ", acc_and_f1[final_name][1])
+            print("Training f1 ", acc_and_f1[final_name][2])
+            print("validation f1 ", acc_and_f1[final_name][3])
 
-        with open(weight_path + path + "train_losses_val_losses.pickle", "wb") as handle:
-            pickle.dump([train_losses, val_losses], handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open("flower_values.pickle","wb") as handle:
+            with open(weight_path + path + "train_losses_val_losses.pickle", "wb") as handle:
+                pickle.dump([train_losses, val_losses], handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(weight_path + "flower_values.pickle", "wb") as handle:
         pickle.dump(acc_and_f1, handle, protocol=pickle.HIGHEST_PROTOCOL)
